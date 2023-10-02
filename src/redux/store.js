e7efc0e42cd03a43edd037ddcaf02c1fd4e2b707
Moyasi Ginko <mrsiyam61@gmail.com>
2023-10-03 04:34:12 +0600

    Add trades reducer in the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from './reducers/counterSlice';
+import tradesReducer from './reducers/tradesSlice';
 
 const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    trades: tradesReducer,
   },
 });
 
