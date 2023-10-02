02c73e4430f304b46f5030e7267e4b3dfc329df3
Moyasi Ginko <mrsiyam61@gmail.com>
2023-10-03 02:53:45 +0600

    Update working directory and create dir. skeleton

--- src/__test__/counterSlice.spec.js
+++ src/__test__/counterSlice.spec.js
@@ -2,7 +2,7 @@ import counterReducer, {
   increment,
   decrement,
   incrementByAmount,
-} from './counterSlice';
+} from '../redux/reducers/counterSlice';
 
 describe('counter reducer', () => {
   const initialState = {
