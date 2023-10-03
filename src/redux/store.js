3d37eb211dfcd83466c9e2179948b4ec04f796be
Moyasi Ginko <mrsiyam61@gmail.com>
2023-10-04 01:11:57 +0600

    Add tradeDetailsSlice in the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import tradesReducer from './reducers/tradesSlice';
+import tradeDetailsReducer from './reducers/tradeDetailsSlice';
 
 const store = configureStore({
   reducer: {
     trades: tradesReducer,
+    tradeDetails: tradeDetailsReducer,
   },
 });
 
