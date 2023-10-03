b99c207a7fba388a6510adb88e5231f310d6c5b0
Moyasi Ginko <mrsiyam61@gmail.com>
2023-10-04 01:58:43 +0600

    Update details page design layout

--- src/pages/TradeDetails.jsx
+++ src/pages/TradeDetails.jsx
@@ -6,51 +6,61 @@ import { fetchTradeDetails } from '../redux/reducers/tradeDetailsSlice';
 const TradesDetails = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
-  const trade = useSelector((state) => state.tradeDetails.trade); // Use state.tradeDetails.trade
+  const trade = useSelector((state) => state.tradeDetails.trade);
 
   useEffect(() => {
-    // Dispatch an action to fetch trade details based on the ID
     dispatch(fetchTradeDetails(id));
   }, [dispatch, id]);
 
-  // Check if trade is null or undefined before accessing its properties
   if (!trade) {
-    return <div>Loading...</div>;
+    return <div className="text-center mt-4">Loading...</div>;
   }
 
   return (
     <div className="container mx-auto p-4">
       <div className="bg-white rounded-lg shadow-md p-4">
-        <img
-          src={trade.image}
-          alt={trade.name}
-          className="w-full h-40 object-cover"
-        />
-        <h2 className="text-xl font-semibold mt-2">{trade.name}</h2>
-        <p className="text-gray-700 text-sm">
-          Location:
-          {trade.location}
-        </p>
-        <p className="text-gray-700 text-sm">
-          Price:$
-          {trade.price}
-        </p>
-        <p className="text-gray-700 text-sm">
-          Duration:
-          {trade.duration}
-        </p>
-        <p className="text-gray-700 text-sm">
-          Type:
-          {trade.trade_type}
-        </p>
-        <p className="text-gray-700 text-sm">
-          Created At:
-          {new Date(trade.created_at).toLocaleString()}
-        </p>
-        <p className="text-gray-700 text-sm">
-          Updated At:
-          {new Date(trade.updated_at).toLocaleString()}
-        </p>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="md:col-span-1">
+            <img
+              src={trade.image}
+              alt={trade.name}
+              className="w-full h-80 object-cover rounded-lg shadow-lg"
+            />
+          </div>
+          <div className="md:col-span-1">
+            <h2 className="text-2xl font-semibold">{trade.name}</h2>
+            <p className="text-gray-600 mt-2">
+              Description:
+              {trade.description}
+            </p>
+            <p className="text-gray-600 mt-2">
+              Location:
+              {trade.location}
+            </p>
+            <p className="text-gray-600 mt-2">
+              Price:$
+              {trade.price}
+            </p>
+            <p className="text-gray-600 mt-2">
+              Duration:
+              {trade.duration}
+            </p>
+            <p className="text-gray-600 mt-2">
+              Type:
+              {trade.trade_type}
+            </p>
+            <div className="mt-4 text-gray-600">
+              <p className="text-sm">
+                Created At:
+                {new Date(trade.created_at).toLocaleString()}
+              </p>
+              <p className="text-sm">
+                Updated At:
+                {new Date(trade.updated_at).toLocaleString()}
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
