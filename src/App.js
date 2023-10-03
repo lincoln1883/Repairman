ddc863e3fbff1197da97d31ff7656ffb251b0306
cosmosha <cosmoshagan@gmail.com>
2023-10-03 18:43:44 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
-import Splash from './pages/Splash';
+import SplashScreen from './pages/Splash';
 import Reservations from './pages/Reservations';
 import Reserve from './pages/Reserve';
 import AddTrades from './pages/AddTrades';
 import DeleteTrade from './pages/DeleteTrade';
-import './App.css';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Splash />} />
-        <Route path="/delete" element={<DeleteTrade />} />
-        <Route path="/add" element={<AddTrades />} />
-        <Route path="/reservations" element={<Reservations />} />
-        <Route path="/reserve" element={<Reserve />} />
-        <Route path="*" element={<h1>Not Found</h1>} />
+      <Route path="/" element={<SplashScreen />} />
+      <Route path="/home" element={<Layout />}>
+        <Route index element={<Reservations />} />
+        <Route path="reserve" element={<Reserve />} />
+        <Route path="add" element={<AddTrades />} />
+        <Route path="delete" element={<DeleteTrade />} />
       </Route>
+      <Route path="*" element={<h1>Not Found</h1>} />
     </Routes>
   );
 }
