232083ec1e1de8a3323a61ef75ea7a19908c0e80
Lincoln Gibson <lincolngibson7@gmail.com>
2023-10-05 11:48:12 -0300

    Change message displayed.

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const NotFound = () => (
   <div className="flex flex-col justify-center items-center mx-auto">
-    <p className="text-2xl">Page not found</p>
+    <p className="text-2xl">Redirecting.....</p>
     <Link to="/">Go back to home</Link>
   </div>
 );
