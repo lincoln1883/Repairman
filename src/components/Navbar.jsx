1503f18035f96ee7f10756401eb457a7eff6f0d3
Lincoln Gibson <lincolngibson7@gmail.com>
2023-10-05 12:23:07 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,12 +11,18 @@ const Navbar = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const links = [
+  const adminLinks = [
     { path: '/trade', text: 'Home' },
     { path: '/trade/reserve', text: 'Reserve' },
     { path: '/trade/reservations', text: 'My Reservations' },
     { path: '/trade/add', text: 'Add Trade' },
     { path: '/trade/delete', text: 'Delete Trade' },
+  ];
+
+  const userLinks = [
+    { path: '/trade', text: 'Home' },
+    { path: '/trade/reserve', text: 'Reserve' },
+    { path: '/trade/reservations', text: 'My Reservations' },
   ];
 
   const handleLogout = () => {
@@ -30,21 +36,44 @@ const Navbar = () => {
     <nav className="w-50 flex flex-col justify-between center border-e-2">
       <div className="flex flex-col justify-around items-center m-3">
         <h1 className="text-3xl">Trade Tracker</h1>
-        <h2 className="text-xl">
-          Welcome
-          {' '}
-          { user ? user.name : 'Guest'}
-        </h2>
-        <div className="navbar-nav mx-auto">
-          <ul>
-            {links.map((link) => (
-              <li key={link.path}>
-                <Link to={link.path}>{link.text}</Link>
-              </li>
-            ))}
-            <button type="button" onClick={handleLogout}>Logout</button>
-          </ul>
-        </div>
+        { user?.role === 'admin' && (
+        <>
+          <h2 className="text-xl">
+            Welcome
+            {' '}
+            { user.name }
+          </h2>
+          <div className="navbar-nav mx-auto">
+            <ul>
+              {adminLinks.map((link) => (
+                <li key={link.path}>
+                  <Link to={link.path}>{link.text}</Link>
+                </li>
+              ))}
+              <button type="button" onClick={handleLogout}>Logout</button>
+            </ul>
+          </div>
+        </>
+        )}
+        { user?.role === 'user' && (
+        <>
+          <h2 className="text-xl">
+            Welcome
+            {' '}
+            { user.name }
+          </h2>
+          <div className="navbar-nav mx-auto">
+            <ul>
+              {userLinks.map((link) => (
+                <li key={link.path}>
+                  <Link to={link.path}>{link.text}</Link>
+                </li>
+              ))}
+              <button type="button" onClick={handleLogout}>Logout</button>
+            </ul>
+          </div>
+        </>
+        )}
       </div>
       <FooterToolbar />
     </nav>
