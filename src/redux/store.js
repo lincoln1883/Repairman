58aa9cb0292286223bfd7ce0443e45bcf92d0875
Lincoln Gibson <lincolngibson7@gmail.com>
2023-10-06 18:23:19 -0300

    Remove unused reducers.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import tradesReducer from './reducers/tradesSlice';
 import tradeDetailsReducer from './reducers/tradeDetailsSlice';
-import usersReducer from './reducers/usersSlice';
 import { logoutReducer } from './reducers/auth/logoutSlice';
 import { registerReducer } from './reducers/auth/registerSlice';
 import { loginReducer } from './reducers/auth/loginSlice';
@@ -10,7 +9,6 @@ const store = configureStore({
   reducer: {
     trades: tradesReducer,
     tradeDetails: tradeDetailsReducer,
-    users: usersReducer,
     register: registerReducer,
     login: loginReducer,
     logout: logoutReducer,
