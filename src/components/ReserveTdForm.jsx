356ba5ee812e26055e15020eece5eef345793f25
Moyasi Ginko <mrsiyam61@gmail.com>
2023-10-07 00:13:47 +0600

    Add catch error for invalid reserve creation

--- src/components/ReserveTdForm.jsx
+++ src/components/ReserveTdForm.jsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { createReservation, resetCreated } from '../redux/reducers/resereveSlice';
+import {
+  createReservation,
+  resetCreated,
+} from '../redux/reducers/resereveSlice';
 import { fetchTrades } from '../redux/reducers/tradesSlice';
 
 const ReserveTdForm = () => {
@@ -9,9 +12,9 @@ const ReserveTdForm = () => {
   const navigate = useNavigate();
   const trades = useSelector((state) => state.trades.trades);
   const created = useSelector((state) => state.reserve.isCreated);
-  console.log('the status is', created);
+  const [errorMessage, setErrorMessage] = useState(''); // State for error message
   const { tradeId } = useParams();
-  console.log('the trade  id is', tradeId);
+
   const [reservationData, setReservationData] = useState({
     trade_id: tradeId,
     date: '',
@@ -33,11 +36,21 @@ const ReserveTdForm = () => {
     return <div>Loading...</div>;
   }
 
-  console.log('the trades are in from', trades);
-
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(createReservation(reservationData));
+    dispatch(createReservation(reservationData))
+      .unwrap()
+      .then(() => {
+        // Handle success here if needed
+      })
+      .catch((rejectedValueOrSerializedError) => {
+        // Handle the error here and set the error message
+        // console.error('Reservation error:', rejectedValueOrSerializedError);
+
+        // Access the error message and set it
+        const errorMessage = rejectedValueOrSerializedError.message || 'Already reserved for this date';
+        setErrorMessage(errorMessage);
+      });
   };
 
   const handleInputChange = (event) => {
@@ -49,39 +62,46 @@ const ReserveTdForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="city">
-          Select a City:
-          <select
-            name="city"
-            id="city"
-            value={reservationData.city}
+    <div>
+      <h2>Create Reservation</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="city">
+            Select a City:
+            <select
+              name="city"
+              id="city"
+              value={reservationData.city}
+              onChange={handleInputChange}
+            >
+              <option value="">Select a City</option>
+              {trades.map((trade) => (
+                <option key={trade.id} value={trade.location}>
+                  {trade.location}
+                </option>
+              ))}
+            </select>
+          </label>
+        </div>
+
+        <div>
+          <input
+            type="date"
+            name="date"
+            value={reservationData.date}
             onChange={handleInputChange}
-          >
-            <option value="">Select a City</option>
-            {trades.map((trade) => (
-              <option key={trade.id} value={trade.location}>
-                {trade.location}
-              </option>
-            ))}
-          </select>
-        </label>
-      </div>
+          />
+        </div>
 
-      <div>
-        <input
-          type="date"
-          name="date"
-          value={reservationData.date}
-          onChange={handleInputChange}
-        />
-      </div>
+        <div>
+          <button type="submit">Create Reservation</button>
+        </div>
+      </form>
 
-      <div>
-        <button type="submit">Create Reservation</button>
-      </div>
-    </form>
+      {errorMessage && (
+        <p style={{ color: 'red', marginTop: '10px' }}>{errorMessage}</p>
+      )}
+    </div>
   );
 };
 
