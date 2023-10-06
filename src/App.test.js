1e0be7a0d1c9996e2abb6b342ef8975348874a91
Lincoln Gibson <lincolngibson7@gmail.com>
2023-10-06 19:51:59 -0300

    Add config to package.json

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import store from './redux/store';
 import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>,
   );
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
 });
