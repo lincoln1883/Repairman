db7a62bf6b3cb0c4b7576d40f085b32ce16da520
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2023-10-09 00:06:48 +0600

    Write 'renders component with reservation' test in showReservation

--- src/test/reservations/ShowReservation.test.js
+++ src/test/reservations/ShowReservation.test.js
@@ -38,3 +38,30 @@ test('renders component with "Reservations" text', () => {
 });
 
 
+test('renders component with reservations', async () => {
+  const reservationsData = [
+    { id: 1, trade: { name: 'Trade 1' }, date: '2023-10-09T12:00:00Z' },
+    { id: 2, trade: { name: 'Trade 2' }, date: '2023-10-10T14:00:00Z' },
+  ];
+
+  const updatedState = {
+    ...initialState,
+    reservations: { reservations: reservationsData },
+  };
+
+  store = mockStore(updatedState);
+
+  render(
+    <Provider store={store}>
+      <MemoryRouter>
+        <ShowReservation />
+      </MemoryRouter>
+    </Provider>,
+  );
+
+  const trade1 = screen.getByText('Trade 1');
+  const trade2 = screen.getByText('Trade 2');
+
+  expect(trade1).toBeInTheDocument();
+  expect(trade2).toBeInTheDocument();
+});
