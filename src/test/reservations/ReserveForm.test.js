be6531243f6aa9add16c322d87a763e16794959f
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2023-10-09 04:14:49 +0600

    Removed 2 unused package

--- src/test/reservations/ReserveForm.test.js
+++ src/test/reservations/ReserveForm.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent, act } from '@testing-library/react';
+import { render, screen} from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
 import configureStore from 'redux-mock-store';
