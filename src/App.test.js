c2e4945437e789e722b2f3664084bd74f0a17410
Moyasi Ginko <mrsiyam61@gmail.com>
2023-10-08 22:41:55 +0600

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom'; // Import MemoryRouter
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders App component', () => {
   const { getByText } = render(
     <Provider store={store}>
-      <App />
-    </Provider>,
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    </Provider>
   );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  // You can add assertions here to check if specific content is present in your app.
+  // For example, if you want to check if the "learn react" text is present:
+  expect(getByText(/learn react/i)).toBeInTheDocument();
 });
