4a3c16c1d7826071da4c1875eb691f452b128ad6
Moyasi Ginko <mrsiyam61@gmail.com>
2023-10-08 22:47:40 +0600

    Update App.test.js

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom'; // Import MemoryRouter
+import { MemoryRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from '../redux/store';
 import App from '../App';
